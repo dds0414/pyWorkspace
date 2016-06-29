824f5ba5d9c473f8aed25b3d9465455048187ed9
yangbo <yangbo@21eline.com>
2016-06-29 18:07:12 +0800

    jtcode

---
+++ JTcode/test.py
@@ -0,0 +1,12 @@
+from recommend import critics
+from recommend import sim_distance
+from recommend import sim_pearson
+from recommend import topMatches
+from recommend import getRecommendations
+
+
+# print sim_distance(critics, "Lisa Rose", "Gene Seymour")
+# print sim_pearson(critics, "Lisa Rose", "Gene Seymour")
+# print topMatches(critics, "Lisa Rose")
+print getRecommendations(critics, "Toby")
+# print critics
--- test/test.py
+++ test/test.py
@@ -1,4 +1,37 @@
+# -*- coding: utf-8 -*-
+import MySQLdb
+import sys
 
-print "test"
 
-print "test2"
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def _doSql(sql):
+    rowL = []
+    try:
+        # 执行sql语句
+        cur.execute(sql)
+        rowL = cur.fetchall()
+        # 提交到数据库执行
+        conn.commit()
+    except:
+        conn.rollback()
+    return rowL
+
+
+conn = MySQLdb.connect(host='localhost', user='root', passwd='', port=3306, charset="utf8")
+cur = conn.cursor()
+conn.select_db('ysd')
+
+
+# file_object = open('country.js').readlines()
+file_object = open('city2.js').readlines()
+for i in file_object:
+    # print i.split(',')[1],i.split(',')[2]
+    arrcity = "INSERT INTO think_cs_city2 (`city_id`,`city_name`) VALUES (" + i.split(',')[2] +"," + i.split(',')[1] + ")"
+    print arrcity
+    _doSql(arrcity)
+
+conn.commit()
+cur.close()
+conn.close()
